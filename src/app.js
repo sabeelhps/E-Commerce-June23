86f94f3527f8d32b277d7948346d3664f56f3958
Sabeel Khan <sabeelhps@gmail.com>
2023-11-19 16:46:42 +0530

    added crud for products

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
 
 //Routes
 const productRoutes = require('./routes/productRoutes');
@@ -9,6 +10,9 @@ const productRoutes = require('./routes/productRoutes');
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true, limit: '10mb', parameterLimit: 5000 }));
+app.use(express.json({ parameterLimit: 5000, limit: '10mb' }));
+app.use(methodOverride('_method'));
 
 app.use(productRoutes);
 
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,10 +2,39 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
 
+// Get all products
 router.get('/products', async(req, res) => {
     const products = await Product.find({});
     res.render('products/index', { products });
 });
+
+// Show new form
+router.get('/products/new', (req, res) => {
+    res.render('products/new');
+});
+
+// Create
+router.post('/products', async (req, res) => {
+    const { name, imageUrl, desc, price } = req.body;
+    await Product.create({ name, imageUrl, desc, price });
+    res.redirect('/products');
+});
+
+// Show
+router.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product });
+});
+
+// Write edit route yourself
+
+// Delete 
+router.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+})
 
 module.exports = router;
 
