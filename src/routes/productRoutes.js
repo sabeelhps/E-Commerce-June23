17978d18dcdd144153416cffe1204f42444026b5
Sabeel Khan <sabeelhps@gmail.com>
2023-12-16 12:35:42 +0530

    removed port 3000 as default

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -7,33 +7,16 @@ const productController = require('../controllers/productController');
 router.route('/products')
     .get(catchAsync(productController.getAllProducts))
     .post(isLoggedIn, catchAsync(productController.createNewProduct));
-        
+
+router.get('/products/new',isLoggedIn, productController.getNewProductsPage);
+
+router.get('/products/:id/edit',isLoggedIn, catchAsync(productController.showEditPage));
+
 router.route('/products/:id')
     .get(catchAsync(productController.showProduct))
     .patch(isLoggedIn, isProductAuthor, catchAsync(productController.updateProduct))
     .delete(isLoggedIn, catchAsync(productController.deleteProduct));
 
-// Get all products
-// router.get('/products', catchAsync(productController.getAllProducts));
-
-// Show new form
-router.get('/products/new',isLoggedIn, productController.getNewProductsPage);
-
-// Create
-// router.post('/products',isLoggedIn, catchAsync(productController.createNewProduct));
-
-// Show
-// router.get('/products/:id',catchAsync(productController.showProduct));
-
-// Write edit route yourself
-// edit route
-router.get('/products/:id/edit',isLoggedIn, catchAsync(productController.showEditPage));
-
-// update
-// router.patch('/products/:id', isLoggedIn, isProductAuthor, catchAsync(productController.updateProduct));
-
-// Delete 
-// router.delete('/products/:id', isLoggedIn, catchAsync(productController.deleteProduct));
 
 module.exports = router;
 
