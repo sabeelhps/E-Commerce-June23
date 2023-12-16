4f299e5916d80a38e90cdcc8a4e7f7faa9f86d27
Sabeel Khan <sabeelhps@gmail.com>
2023-12-16 11:36:44 +0530

    removed port 3000 as default

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 const DB = require('./data-source');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 (async () => {
     try {
